b10c029a78602d0e645d3a1ef6e564253c85b89d
Samuel D' Siqueira <41975168+samueldsiqueira@users.noreply.github.com>
2024-11-05 12:01:26 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # la-fiesta
+
 Aplicativo para criação de convites de festa e compartilhamento para lista de contatos;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-	return <h1>Kamilly, se você me ama, da uma risadinha!!!</h1>;
+  return <h1>Kamilly, se você me ama, da uma risadinha!!!</h1>;
 }
 export default Home;
