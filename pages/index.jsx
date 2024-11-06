a8f5dff59df0bc2b240b5e19ccd7131ac8a0e173
Samuel D' Siqueira <41975168+samueldsiqueira@users.noreply.github.com>
2024-11-06 16:02:31 +0000

    alterado `index.js` para `index.jsx`

---
+++ pages/index.jsx
@@ -0,0 +1,59 @@
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Projeto Vem pra Festa</h1>
+        <div>
+          <span>
+            projeto focado em ser uma pequena página onde você monta o convite e
+            após essa <b>montagem</b> ao finalizar é gerado um valor para fazer
+            a distribuição
+          </span>
+        </div>
+        <div>
+          <ul>
+            <h3>O que precisa ser feito?</h3>
+            <li>
+              <p>Definir cor do projeto;</p>
+            </li>
+            <li>
+              <p>estilização;</p>
+            </li>
+            <li>
+              <p>formulário;</p>
+            </li>
+            <li>
+              <p>metodo de envio dos convites;</p>
+            </li>
+            <li>
+              <p>persistir informação;</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <h3>O que precisa ser protegido?</h3>
+            <li>
+              <p>Informações sobre a festa;</p>
+            </li>
+            <li>
+              <p>dados de pagamento;</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <h3>
+              O que precisa ser repetido para as NOVAS pessoas acessarem o
+              espaço?
+            </h3>
+            <li>
+              <p>Layout rápido ;</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Home;
