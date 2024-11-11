bc28211d38130787147c6c51734fcd553973438e
Samuel D' Siqueira <41975168+samueldsiqueira@users.noreply.github.com>
2024-11-11 11:25:35 +0000

    adiciona configuração modulo pg, configuração de um yaml, configuração de conexão

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-const status = (req, res) => {
+import database from "../../../../infra/postgres.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({ message: "complete" });
-};
+}
 
 export default status;
