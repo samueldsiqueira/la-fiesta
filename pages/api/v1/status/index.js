305c1dd2b11f6097fab9d7914a92ed402cd871bb
Samuel D' Siqueira <41975168+samueldsiqueira@users.noreply.github.com>
2024-11-12 13:52:55 +0000

    add

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/postgres.js";
+import database from "./infra/postgres.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
